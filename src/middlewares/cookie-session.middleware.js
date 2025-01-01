47915393c394463866c83c9d1012f5f5769e03a7
GhostOrder28 <jtrvic7@gmail.com>
2024-12-31 22:01:38 -0500

    domain prop added to cookie-session options

--- src/middlewares/cookie-session.middleware.js
+++ src/middlewares/cookie-session.middleware.js
@@ -1,10 +1,11 @@
 const cookieSessionOptions = {
   name: 'session',
-  sameSite: 'none',
+  sameSite: 'Strict',
   secure: true,
   // secureProxy: true,
   maxAge: 24 * 60 * 60 * 1000,
-  keys: [ process.env.COOKIE_KEY_1, process.env.COOKIE_KEY_2 ]
+  keys: [ process.env.COOKIE_KEY_1, process.env.COOKIE_KEY_2 ],
+  domain: process.env.CLIENT_BASE_URL
 }
 
 module.exports = cookieSessionOptions;
