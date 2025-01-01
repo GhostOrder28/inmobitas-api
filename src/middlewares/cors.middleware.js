892677db4dea59f8c4c59ab3d3cf6f76aa6e004c
GhostOrder28 <jtrvic7@gmail.com>
2025-01-01 03:51:00 -0500

    code cleaning

--- src/middlewares/cors.middleware.js
+++ src/middlewares/cors.middleware.js
@@ -2,15 +2,8 @@ const corsOptions = {
   origin: [
     `https://${process.env.CLIENT_BASE_URL}`,
     `https://www.${process.env.CLIENT_BASE_URL}`,
-    ...(
-      process.env.CLIENT_PRIVATE_URL ? [ 
-        `https://${process.env.CLIENT_PRIVATE_URL}`,
-      ] : []
-    )
   ],
   credentials: true,
 }
-
-console.log('corsOptions: ', corsOptions);
 
 module.exports = corsOptions
